67de8aba25cdd74d27b03f470bb5f7c4ab3916e6
maciejgaleja <maciej.galeja@o2.pl>
2017-12-03 16:45:53 +0100

    Added position check + some refactoring

--- mavioso/GeoCoordinate.py
+++ mavioso/GeoCoordinate.py
@@ -1,8 +1,23 @@
+import math
+
 class GeoCoordinate:
     def __init__(self, latitude, longitude, altitude):
         self.latitude = float(latitude)
         self.longitude = float(longitude)
         self.altitude = float(altitude)
+
+    """ Returns distance to another coordinate in meters"""
+    def distance_to(self, other):
+        diff_lat = other.latitude - self.latitude
+        diff_lat = (111132.954 - 559.822 * math.cos(2 * self.latitude) + 1.175 * math.cos(4 * self.latitude))*diff_lat
+        diff_lon = (other.longitude - self.longitude)
+        diff_lon = 111132.954 * math.cos(self.latitude) * diff_lon
+        diff_alt = (other.altitude - self.altitude)
+        diff_lat = math.pow(diff_lat, 2)
+        diff_lon = math.pow(diff_lon, 2)
+        diff_alt = math.pow(diff_alt, 2)
+        diff_tot = math.sqrt(diff_lat + diff_lon + diff_alt)
+        return diff_tot
 
     def __str__(self):
         string = "{0} {1} {2}".format(self.latitude, self.longitude, self.altitude)
@@ -10,5 +25,5 @@ class GeoCoordinate:
 
     @staticmethod
     def from_mav_state(mav_state):
-        coord = GeoCoordinate(mav_state.latitude, mav_state.longitude, mav_state.altitude)
+        coord = GeoCoordinate(mav_state["latitude"], mav_state["longitude"], mav_state["altitude"])
         return coord
--- mavioso/MAV.py
+++ mavioso/MAV.py
@@ -1,8 +1,14 @@
 import logging
+import time
 
 from MissionPlanner.Utilities import Locationwp
 
 import mavioso.MaviosoExceptions
+import mavioso.GeoCoordinate as GeoCoordinate
+
+WAIT_WAYPOINT_SLEEP_TIME = 500
+VTOL_MODE_PLANE = 0
+VTOL_MODE_QUAD = 1
 
 class MAV:
     def __init__(self, Script, MAV, MAVLink, cs):
@@ -10,12 +16,17 @@ class MAV:
         self.mav = MAV
         self.mavlink = MAVLink
         self.cs = cs
+        self.position_check_threshold = 50
+
 
     def currentstate(self):
-        ret = {"lat": float(self.cs.lat), "lng": float(self.cs.lng), "alt": float(self.cs.alt)}
+        """Return current state as dictionary"""
+        ret = {"latitude": float(self.cs.lat), "longitude": float(self.cs.lng), "altitude": float(self.cs.alt)}
         return ret
 
     def arm(self):
+        """Arm MAV
+        :return True on success, False if MAV is already armed"""
         if self.cs.armed:
             logging.info("Already armed, ignoring command...")
             return False
@@ -26,6 +37,8 @@ class MAV:
         return status
 
     def disarm(self):
+        """Disarm MAV
+        :return True on success, False if MAV is already disarmed"""
         if self.cs.armed is False:
             logging.info("Already disarmed, ignoring command...")
             return False
@@ -36,6 +49,9 @@ class MAV:
         return status
 
     def takeoff(self, alt):
+        """Issue Takeoff command
+        :param alt: Altitude to be obtained after takeoff (no horizontal movement is assumed)
+        :return True on success, False otherwise"""
         if self.cs.armed is False:
             raise mavioso.MaviosoExceptions.NotArmedException('Takeoff failed, UAV is not armed')
         if self.cs.mode.upper() != 'GUIDED':
@@ -47,22 +63,57 @@ class MAV:
             raise mavioso.MaviosoExceptions.MaviosoException('unexpected error while taking off')
         return status
 
-    def set_waypoint(self, coordinate):
+    def set_waypoint(self, coordinate, should_wait=False, timeout=-1):
+        """Set GUIDED mode waypoint
+        :param coordinate: GeoCoordinate object describing waypoint
+        :param should_wait: (bool) Wait until waypoint is reached? (Basing on is_position_ok function)
+        :param timeout: timeout in seconds (works if should_wait is True): -1 to disable"""
         # TODO: it probably does not like coordinate argument that equals 0.0. Setting it to 0.01 works fine
         wp1 = Locationwp().Set(coordinate.latitude, coordinate.longitude, coordinate.altitude,
                                int(self.mavlink.MAV_CMD.WAYPOINT))
         self.mav.setGuidedModeWP(wp1, True)
         logging.info("MAV: set_waypoint() {0}".format(str(coordinate)))
+        timeout_occurred = False
+        if should_wait:
+            timeout_occurred = self.wait_waypoint(coordinate, timeout=timeout)
+        return timeout_occurred
+
+    def wait_waypoint(self, coordinate, threshold=None, timeout=-1):
+        """Wait until MAV reaches specified waypoint
+        :param coordinate: GeoCoordinate object, a waypoint to check
+        :param threshold: maximal distance from waypoint to assume waypoint is reached
+        :param timeout: timeout in seconds (-1 to disable)"""
+        time_begin = time.time()
+        timeout_occurred = False
+        while not(self.is_position_ok(coordinate, threshold)):
+            self.script.Sleep(WAIT_WAYPOINT_SLEEP_TIME)
+            current_time = time.time()
+            if (timeout > 0) and (current_time - time_begin) > timeout:
+                timeout_occurred = True
+                break
+        return timeout_occurred
+
+    def is_position_ok(self, coordinate, threshold=None):
+        """Check if MAV is within range of specified coordinate"""
+        thr = self.position_check_threshold
+        if threshold is not None:
+            thr = threshold
+        current_pos = GeoCoordinate.GeoCoordinate.from_mav_state(self.currentstate())
+        dist = coordinate.distance_to(current_pos)
+        logging.debug("MAV: is_position_ok: distance = {0}".format(dist))
+        return dist < thr
 
     def set_mode(self, mode):
         status = self.mav.setMode(mode)
         return status
 
-    def q_loiter(self):
-        self.mav.setParam("Q_GUIDED_MODE", 1)       #when the loiter is ended, we have to remember to change Q_GUIDED_MODE to 0, if we want it to be 0
+    def set_VTOL_mode(self, quadmode):
+        """Put MAV into quadrotor or plane mode
+        :param quadmode: mode to set (1 for quadrotor, 0 for plane)"""
+        self.mav.setParam("Q_GUIDED_MODE", quadmode)
         wp1 = Locationwp().Set(self.cs.lat, self.cs.lng, self.cs.alt, int(self.mavlink.MAV_CMD.WAYPOINT))
         self.mav.setGuidedModeWP(wp1, True)
-        logging.info("qloiter")
+        logging.info("MAV: VTOL mode: {0}".format(quadmode))
 
     def set_circle_radius(self, radius):            #set circle radius in loiter and guided (circles after reaching the waypoint) mode
         status = self.mav.setParam("WP_LOITER_RAD", radius)
